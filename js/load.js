6e49cd79d0dc08b8ccd0e3c260e3e046df9554ba
Tomas Lingotti <tomaslingotti@gmail.com>
2022-01-26 01:25:53 -0300

    playing with before install prompt

--- js/load.js
+++ js/load.js
@@ -8,7 +8,7 @@ if ("serviceWorker" in navigator) {
 }
 
 // install button
-let deferredPrompt;
+let deferredPrompt = null;
 
 window.addEventListener('beforeinstallprompt', function(e) {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -16,24 +16,6 @@ window.addEventListener('beforeinstallprompt', function(e) {
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
     console.log(deferredPrompt)
-});
-
-// Installation must be done by a user gesture! Here, the button click
-btnAdd.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-        .then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
 });
 
 function install() {
